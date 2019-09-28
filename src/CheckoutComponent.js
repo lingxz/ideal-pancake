9ed9948f93f07b3605e0edc8d14277202fe231c4
William Haw <william.haw@agoda.com>
2019-09-29 00:18:06 +0800

    Change checkout page to table

--- src/CheckoutComponent.js
+++ src/CheckoutComponent.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
-import { Button, Box, Card, Media, Level, Heading } from "react-bulma-components/full";
-import { Link } from 'react-router-dom';
+import { Button, Heading, Table } from "react-bulma-components/full";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDollarSign } from '@fortawesome/free-solid-svg-icons'
 
+library.add(faDollarSign);
 
 class CheckoutComponent extends Component {
   constructor(props) {
@@ -11,7 +14,7 @@ class CheckoutComponent extends Component {
     } = this.props;
     this.state = {
       user: params.userId,
-      totalCost: 123,
+      totalCost: 100,
       matches: {}
     };
 
@@ -47,17 +50,31 @@ class CheckoutComponent extends Component {
     return (
       <div style={{ margin: "0.75rem" }}>
         <Heading size={2}>Welcome back, {this.state.user}!</Heading>
-        {Object.keys(this.state.matches).map(sellerId => {
-          return (
-            <Card rounded outlined style={{ borderRadius: "6px", margin: "1.25rem" }}>
-              <Card.Content card-content-padding={"1.5rem"}>
-                  <Heading size={3}>{this.state.matches[sellerId].name}</Heading>
-                  <p>{this.state.matches[sellerId].text}</p>
-              </Card.Content>
-            </Card>
-          );
-        })}
-        <Button><Link>{`Total Cost: $${this.state.totalCost}`}. Checkout now!</Link></Button>
+        <Table>
+          <tbody>
+            {Object.keys(this.state.matches).map((sellerId, index) => {
+              return (
+                <tr key={sellerId}>
+                  <th>
+                    {index + 1}
+                  </th>
+                  <td>
+                    {this.state.matches[sellerId].name}
+                  </td>
+                  <td>
+                    <FontAwesomeIcon icon="dollar-sign" />50
+                  </td>
+                </tr>
+              );
+            })}
+            <tr>
+              <td></td>
+              <td>Total</td>
+              <td><FontAwesomeIcon icon="dollar-sign" />100</td>
+            </tr>
+          </tbody>
+        </Table>
+        <Button color='success'>Checkout</Button>
       </div>
     );
   }
