074511b00430dd5e88ad4503eec5d8aa16ea568f
William Haw <william.haw@agoda.com>
2019-09-28 13:58:01 +0800

    Add data and disply of budget and items for buyer

--- src/ProfileComponent.js
+++ src/ProfileComponent.js
@@ -1,30 +1,40 @@
 import React, { Component } from 'react';
 
-class ProfileComponent extends Component{
+class ProfileComponent extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
             profile: {
-                "name": "user"
+                "name": "user",
+                "role": "buyer",
+                "budget": "100",
+                "items": []
             }
-        } ;
+        };
 
         const { match: { params } } = this.props;
         fetch('/user/' + params.userId + '/profile.json')
-        .then(res => res.json())
-        .then((data) => {
-            this.setState({profile: data})
-            console.log(this.state)
-        })
-        .catch(console.log)
-      }
-
-    render(){
-        return(
-            <h1>Hi, {this.state.profile.name}</h1>
-        )
+            .then(res => res.json())
+            .then((data) => {
+                this.setState({ profile: data })
+            })
+            .catch(console.log)
     }
-}
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.profile.name}</h1>
+                <h2>${this.state.profile.budget}</h2>
+                <ul>
+                    {this.state.profile.items.map(item => {
+                        return <li>{item}</li>
+                    })}
+            </ul>
+            </div>
+                )
+            }
+        }
 
 export default ProfileComponent;
