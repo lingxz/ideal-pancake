5a919ca9f3ed1095e5f785432911cbc5b737b820
William Haw <william.haw@agoda.com>
2019-09-29 00:53:05 +0800

    Fix formatting

--- src/PickerComponent.js
+++ src/PickerComponent.js
@@ -7,38 +7,38 @@ const api_endpoint = process.env.API_URL ? process.env.API_URL : "http://localho
 class PickerComponent extends Component {
 
   constructor(props) {
-      super(props);
-      const { match: { params } } = this.props;
+    super(props);
+    const { match: { params } } = this.props;
 
-      let filteredData = [];
-      if (params.userId.startsWith("buyer")) {
-        // is a buyer
-        filteredData = data.filter(item => item.id.startsWith("seller"));
-      } else {
-        // is a seller, wants to see buyer
-        filteredData = data.filter(item => item.id.startsWith("buyer"));
-      }
+    let filteredData = [];
+    if (params.userId.startsWith("buyer")) {
+      // is a buyer
+      filteredData = data.filter(item => item.id.startsWith("seller"));
+    } else {
+      // is a seller, wants to see buyer
+      filteredData = data.filter(item => item.id.startsWith("buyer"));
+    }
 
-      this.state = { 
-        "userId": params.userId,
-        "cards": filteredData
-      }
-      // let endpoint = "/sellers";
-      // if (params.userId.startsWith("seller")) {
-      //   endpoint = "/buyers";
-      // }
-      // fetch(api_endpoint + endpoint)
-      //     .then(res => res.json())
-      //     .then((data) => {
-      //       console.log("cards", data.results);
-      //       this.setState({ "cards": data.results })
-      //     })
-      //     .catch(console.log)
+    this.state = {
+      "userId": params.userId,
+      "cards": filteredData
+    }
+    // let endpoint = "/sellers";
+    // if (params.userId.startsWith("seller")) {
+    //   endpoint = "/buyers";
+    // }
+    // fetch(api_endpoint + endpoint)
+    //     .then(res => res.json())
+    //     .then((data) => {
+    //       console.log("cards", data.results);
+    //       this.setState({ "cards": data.results })
+    //     })
+    //     .catch(console.log)
   }
 
   render() {
-      return <Deck userId={this.state.userId} cards={this.state.cards}/>
-          }
-      }
+    return <Deck userId={this.state.userId} cards={this.state.cards} />
+  }
+}
 
 export default PickerComponent;
