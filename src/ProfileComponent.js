95bda27ff0867fbe8a6738001fa89f1ca523e4c8
William Haw <william.haw@agoda.com>
2019-09-28 17:28:12 +0800

    Add profile picture for buyer

--- src/ProfileComponent.js
+++ src/ProfileComponent.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGift, faDollarSign } from '@fortawesome/free-solid-svg-icons'
-import { Button, Box, Card, Media } from "react-bulma-components/full";
+import { Button, Box, Card, Media, Level } from "react-bulma-components/full";
+import { link } from 'fs';
 
 library.add(faGift, faDollarSign);
 
@@ -32,18 +33,31 @@ class ProfileComponent extends Component {
         return (
             <Box box-padding={"1.25rem"}>
                 <Card>
-                    <Card.Content card-content-padding={"1.5rem"}>
-                        <h1>{this.state.profile.name}</h1>
-                        <h2><FontAwesomeIcon icon="dollar-sign" />{this.state.profile.budget}</h2>
-                        {this.state.profile.items.map(item => {
-                            return (
-                                <Media key={item.id}>
-                                    <Media.Item position="left"><FontAwesomeIcon icon="gift" /></Media.Item><Media.Item>{item.name}</Media.Item>
-                                </Media>
-                            )
-                        })}
-                        <Button>Match!</Button>
-                    </Card.Content>
+                    <Level>
+                        <Level.Item>
+                            <Card.Image src={"/assets/azure-lin.jpg"} alt="profile picture" ratio='1by1' style={{ maxWidth: 320 }} is-flex="true" />
+                        </Level.Item>
+                    </Level>
+                    <Level>
+                        <Level.Item>
+                            <Card.Content card-content-padding={"1.5rem"}>
+                                <h1>{this.state.profile.name}</h1>
+                                <h2><FontAwesomeIcon icon="dollar-sign" />{this.state.profile.budget}</h2>
+                                {this.state.profile.items.map(item => {
+                                    return (
+                                        <Media key={item.id}>
+                                            <Media.Item position="left"><FontAwesomeIcon icon="gift" /></Media.Item><Media.Item>{item.name}</Media.Item>
+                                        </Media>
+                                    )
+                                })}
+                            </Card.Content>
+                        </Level.Item>
+                    </Level>
+                    <Level>
+                        <Level.Item>
+                            <Button>Match!</Button>
+                        </Level.Item>
+                    </Level>
                 </Card>
             </Box>
         )
