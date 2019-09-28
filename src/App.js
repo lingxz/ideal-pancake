948a2840fa26316a1f0d914006fbc1edaa10634d
William Haw <william.haw@agoda.com>
2019-09-28 12:54:34 +0800

    Fix formatting in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { HashRouter as Router,
+import {
+  HashRouter as Router,
   Route,
-  Link } from 'react-router-dom'
+  Link
+} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 import ProfileComponent from './ProfileComponent';
@@ -12,17 +14,17 @@ function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
-      <ul>
-            <li>
-              <Link to="/profile/buyer"> Buyer Profile </Link>
-            </li>
-            <li>
-              <Link to="/picker/buyer"> Picker </Link>
-            </li>
-            <li>
-              <Link to="/checkout/buyer"> Checkout </Link>
-            </li>
-          </ul>
+        <ul>
+          <li>
+            <Link to="/profile/buyer"> Buyer Profile </Link>
+          </li>
+          <li>
+            <Link to="/picker/buyer"> Picker </Link>
+          </li>
+          <li>
+            <Link to="/checkout/buyer"> Checkout </Link>
+          </li>
+        </ul>
 
         <Route exact path='/profile/:userId' component={ProfileComponent}></Route>
         <Route exact path='/picker/:userId' component={PickerComponent}></Route>
