c5f4e90d618f494fc4b9ad59fa9b506ff3ae28c8
William Haw <william.haw@agoda.com>
2019-09-28 14:34:05 +0800

    Add NotFoundComponent

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import {
   HashRouter as Router,
   Route,
-  Link
+  Link,
+  Switch
 } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 import ProfileComponent from './ProfileComponent';
 import PickerComponent from './PickerComponent';
 import CheckoutComponent from './CheckoutComponent';
+import NotFoundComponent from './NotFoundComponent';
 
 function App() {
   return (
@@ -26,9 +28,12 @@ function App() {
           </li>
         </ul>
 
-        <Route exact path='/profile/:userId' component={ProfileComponent}></Route>
-        <Route exact path='/picker/:userId' component={PickerComponent}></Route>
-        <Route exact path='/checkout/:userId' component={CheckoutComponent}></Route>
+        <Switch>
+          <Route exact path='/profile/:userId' component={ProfileComponent}></Route>
+          <Route exact path='/picker/:userId' component={PickerComponent}></Route>
+          <Route exact path='/checkout/:userId' component={CheckoutComponent}></Route>
+          <Route component={NotFoundComponent}></Route>
+        </Switch>
       </div>
     </Router>
   );
---
+++ src/NotFoundComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NotFoundComponent() {
+    return (
+        <div>
+            <h1>Not Found</h1>
+            The requested resource could not be found.
+        </div>
+    )
+
+}
+
+export default NotFoundComponent;
