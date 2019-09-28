a62a0e2be1b2f239e1bc77e5097a1712a14b6119
William Haw <william.haw@agoda.com>
2019-09-28 20:17:17 +0800

    Use button in RootComponent

--- src/RootComponent.js
+++ src/RootComponent.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
-
+import { Link } from 'react-router-dom';
+import { Button } from "react-bulma-components/full";
 class RootComponent extends Component {
 
     render() {
         return (
             <ul>
-                <li><Link to={"/profile/buyer"}>Buyer Profile</Link></li>
-                <li><Link to={"/profile/seller1"}>Seller 1 Profile</Link></li>
-                <li><Link to={"/profile/seller2"}>Seller 2 Profile</Link></li>
-                <li><Link to={"/picker/buyer"}>Buyer Picker</Link></li>
-                <li><Link to={"/picker/seller1"}>Seller 1 Picker</Link></li>
+                <li><Button><Link to={"/profile/buyer"}>Buyer Profile</Link></Button></li>
+                <li><Button><Link to={"/profile/seller1"}>Seller 1 Profile</Link></Button></li>
+                <li><Button><Link to={"/profile/seller2"}>Seller 2 Profile</Link></Button></li>
+                <li><Button><Link to={"/picker/buyer"}>Buyer Picker</Link></Button></li>
+                <li><Button><Link to={"/picker/seller1"}>Seller 1 Picker</Link></Button></li>
             </ul>
         )
     }
