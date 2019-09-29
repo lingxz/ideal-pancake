4875a807384acb27d23d4f4d8e231f46a4fddb8f
William Haw <william.haw@agoda.com>
2019-09-29 11:48:22 +0800

    Add home icon to each page

--- src/App.js
+++ src/App.js
@@ -2,18 +2,25 @@ import React from 'react';
 import {
   HashRouter as Router,
   Route,
+  Link,
   Switch
 } from 'react-router-dom';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faHome } from '@fortawesome/free-solid-svg-icons'
 import ProfileComponent from './ProfileComponent';
 import PickerComponent from './PickerComponent';
 import CheckoutComponent from './CheckoutComponent';
 import NotFoundComponent from './NotFoundComponent';
 import RootComponent from './RootComponent';
 
+library.add(faHome);
+
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
+      <Link to={"/"} style={{color: "#ffffff", position: "fixed", top: "1rem", left: "0.5rem", zIndex: 100}}><FontAwesomeIcon icon="home"></FontAwesomeIcon></Link>
       <div className="App">
         <Switch>
           <Route exact path='/' component={RootComponent}></Route>
